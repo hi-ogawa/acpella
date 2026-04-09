4bcb66f073934390cd7d2d9d6d8efcbae33ca40e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-04-09 23:29:27 +0900

    test: set test.dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  test: {
+    dir: "./src",
+  },
+  fmt: {},
   staged: {
     "*": "vp check --no-lint --fix",
   },
-  fmt: {},
 });
