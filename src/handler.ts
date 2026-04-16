83d73a400d70fca9960862cd3c1406ed17e75695
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-04-16 10:27:33 +0900

    nit

--- src/handler.ts
+++ src/handler.ts
@@ -199,7 +199,6 @@ agent session id: ${stateSession.agentSessionId ?? "none"}
     },
     {
       path: ["load"],
-      // TODO: support <agent:sessionId> only
       usage: "/session load <sessionId|agent:sessionId>",
       summary: "Load an existing agent session.",
       match: "prefix",
