ed581b0fe8fc7a92f58cb409e8408c7146153789
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-04-25 17:14:13 +0900

    refactor: simplify parse cli error (#128)

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,5 +1,3 @@
-import { Result } from "./utils.ts";
-
 type ParsedCli = {
   command: string;
   args: string[];
@@ -9,24 +7,24 @@ export function parseCli(options: {
   argv: string[];
   commands: string[];
   defaultCommand?: string;
-}): Result<ParsedCli, string> {
+}): ParsedCli {
   const [inputCommand, ...args] = options.argv.slice(2);
   const command = inputCommand ?? options.defaultCommand;
 
   if (command === "-h" || command === "--help") {
-    return Result.ok({
+    return {
       command: "help",
       args: [],
-    });
+    };
   }
   if (!command) {
-    return Result.err("Missing command");
+    throw new Error("Missing command");
   }
   if (!options.commands.includes(command)) {
-    return Result.err(`Unknown command: ${command}`);
+    throw new Error(`Unknown command: ${command}`);
   }
-  return Result.ok({
+  return {
     command,
     args,
-  });
+  };
 }
