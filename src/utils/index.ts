a6272b894c94bdf8cde4e80de2592e0933cf21b1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-04-29 12:53:15 +0900

    refactor: move utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,6 +5,8 @@ export function truncateString(s: string, limit: number) {
   return s;
 }
 
+export * from "./timing.ts";
+
 export function addIndent({ text, indent }: { text: string; indent: string }): string {
   return text
     .split(/\r?\n/)
@@ -122,63 +124,4 @@ export class DefaultMap<K, V> extends Map<K, V> {
     }
     return super.get(key)!;
   }
-}
-
-export type Throttler = ReturnType<typeof throttle>;
-
-export function throttle(fn: () => void, ms: number) {
-  let timeout: ReturnType<typeof setTimeout> | undefined;
-  function schedule() {
-    if (typeof timeout === "undefined") {
-      timeout = setTimeout(() => {
-        timeout = undefined;
-        fn();
-      }, ms);
-    }
-  }
-
-  function cancel() {
-    if (typeof timeout !== "undefined") {
-      clearTimeout(timeout);
-      timeout = undefined;
-    }
-  }
-
-  function flush() {
-    cancel();
-    fn();
-  }
-
-  return { schedule, cancel, flush };
-}
-
-export type Debouncer = ReturnType<typeof debounce>;
-
-export function debounce(fn: () => void, ms: number) {
-  let timeout: ReturnType<typeof setTimeout> | undefined;
-
-  function schedule() {
-    cancel();
-    timeout = setTimeout(() => {
-      timeout = undefined;
-      fn();
-    }, ms);
-  }
-
-  function cancel() {
-    if (typeof timeout !== "undefined") {
-      clearTimeout(timeout);
-      timeout = undefined;
-    }
-  }
-
-  function flush() {
-    const shouldRun = typeof timeout !== "undefined";
-    cancel();
-    if (shouldRun) {
-      fn();
-    }
-  }
-
-  return { schedule, cancel, flush };
 }
